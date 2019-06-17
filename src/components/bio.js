b78fa2b699048ee3dee7fb81744da5a3d2a010c0
Carlos Nah <cnah27@gmail.com>
2019-06-17 15:26:15 +0000

    Added profile pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,8 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives in Monrovia and is passionate about helping young developer in JavaScript and programming paradigm.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
--- src/components/layout.js
+++ src/components/layout.js
@@ -63,9 +63,9 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()},   Developed by <i>
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://gitjub.com/ra9">Carlos S. Nah Jr.</a>
         </footer>
       </div>
     )
